a29684d478aeb364111377d1cf923d80eb5ab3bb
Bo Kim <bohyung0110@gmail.com>
2022-03-17 14:20:18 -0700

    version 3

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use('/',express.static(path.join(__dirname, "../public")));
 
 const port = process.env.PORT || 4004;
 
